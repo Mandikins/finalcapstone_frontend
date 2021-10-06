c8cfd46c566ac98872624b955ce1bc4877d7b430
Amanda Garrison <80993434+Mandikins@users.noreply.github.com>
2021-10-05 19:13:06 -0500

    Add files via upload

---
+++ src/components/layout/Routes.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+import { Redirect, Route, Switch } from "react-router-dom";
+import Dashboard from "../dashboard/Dashboard";
+import NotFound from "../errors/NotFound";
+import { today } from "../../utils/date-time";
+import ReservationForm from "../reservations/ReservationForm";
+import TablesForm from "../tables/TableForm";
+import Seating from "../dashboard/Seating";
+import FindByNumber from "../dashboard/FindByNumber";
+
+/**
+ * Defines all the routes for the application.
+ *
+ * You will need to make changes to this file.
+ *
+ * @returns {JSX.Element}
+ */
+function Routes() {
+  return (
+    <Switch>
+      <Route exact={true} path="/">
+        <Redirect to={"/dashboard"} />
+      </Route>
+      <Route exact={true} path="/reservations">
+        <Redirect to={"/dashboard"} />
+      </Route>
+      <Route exact={true} path="/reservations/new">
+        <ReservationForm type="Create a new"/>
+      </Route>
+      <Route path="/dashboard">
+        <Dashboard date={today()} />
+      </Route>
+      <Route path="/tables/new">
+        <TablesForm />
+      </Route>
+      <Route path="/reservations/:reservation_id/seat">
+        <Seating />
+      </Route>
+      <Route path="/reservations/:reservation_id/edit">
+        <ReservationForm type="Edit"/>
+      </Route>
+      <Route path="/search">
+        <FindByNumber />
+      </Route>
+      <Route>
+        <NotFound />
+      </Route>
+    </Switch>
+  );
+}
+
+export default Routes;
